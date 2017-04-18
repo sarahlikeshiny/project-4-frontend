22968e073a62411536e9f625162ac2640be1c6de
sarahlikeshiny <sarah.g.thomas@gmail.com>
2017-04-18 12:30:10 +0100

    map markers working

--- src/js/directives/googlemap.js
+++ src/js/directives/googlemap.js
@@ -18,58 +18,39 @@ function googleMap($window) {
         zoom: 3,
         center: $scope.center
       });
-      new $window.google.maps.Marker({
-        position: $scope.center,
-        map: map,
-        animation: $window.google.maps.Animation.DROP
-      });
-
-  //location markers
+      console.log($scope.locations);
+      // let infoWindow = null;
+      let marker = null;
+      const markers = [];
 
 
-      // let markers = [];
-      //
-      // function addSensors() {
-      //   $scope.locations.forEach(function(location){
-      //     console.log('locations', location);
-      //     location.latitude = $scope.locations.lat;
-      //     location.longitude = $scope.locations.long;
-      //
-      //     addMarker(location);
-      //     console.log(location);
-      //   });
 
+      function addLocations() {
+        $scope.locations.forEach(function(location){
+          console.log(location)
+          location.latitude = location.lat;
+          location.longitude = location.lng;
 
-    //     function addMarker(location) {
-    //       const latLng = { lat: location.latitude, lng: location.longitude };
-    //       // console.log(latLng);
-    //       marker = new google.maps.Marker({
-    //         position: latLng,
-    //         map,
-    //         animation: google.maps.Animation.DROP
-    //         // icon: '/assets/restaurant.svg' // Adding a custom icon
-    //       });
-    //       markers.push(marker);
-    //     }
-    //   }
-    // }
+          addMarker(location);
+        });
+      }
+
+      addLocations();
+
+      function addMarker(location) {
+        const latLng = { lat: location.latitude, lng: location.longitude };
+        // console.log(latLng);
+        marker = new google.maps.Marker({
+          position: latLng,
+          map: map,
+          animation: google.maps.Animation.DROP
+          // icon: '/assets/restaurant.svg' // Adding a custom icon
+        });
+
+        markers.push(marker);
+
+      }
     }
   };
   return directive;
 }
-//   function eqfeedCallback(results) {//pass in the data to add to the map.
-//     console.log(results);
-//     var heatmapData = [];//creates an array of lat longs
-//     for (var i = 0; i < results.features.length; i++) {
-//       var coords = results.features[i].geometry.coordinates;
-//       var latLng = new google.maps.LatLng(coords[1], coords[0]);
-//       heatmapData.push(latLng);
-//     }
-//     new $window.google.maps.visualization.HeatmapLayer({//maps out the lat longs
-//       data: heatmapData,
-//       dissipating: false,
-//       map: map
-//     });
-//   }
-//   eqfeedCallback();
-// }
